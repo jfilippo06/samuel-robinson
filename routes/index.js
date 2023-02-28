b46ea61c7881751833c22507d54a89bd6d2f23b7
jfilippo06 <jfilippo04@gmail.com>
2023-02-28 04:05:21 +0100

    login

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const homeRouter = require("./home");
+const authRouter = require("./auth");
+
+router.use("/", homeRouter);
+router.use("/auth", authRouter);
 
 module.exports = router;
