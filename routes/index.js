9fd156e411db8f59ef2992728b7729c57f968b42
jfilippo06 <jfilippo04@gmail.com>
2023-02-28 14:11:55 +0100

    admin

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const homeRouter = require("./home");
 const authRouter = require("./auth");
+const adminRouter = require("./admin");
 
 router.use("/", homeRouter);
 router.use("/auth", authRouter);
+router.use("/admin", adminRouter);
 
 module.exports = router;
