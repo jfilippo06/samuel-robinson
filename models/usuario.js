217846abf351de16955dbf1ec7241ba849e1bdad
jfilippo06 <jfilippo04@gmail.com>
2023-02-28 16:18:00 +0100

    sequelize

---
+++ models/usuario.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Usuario extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Usuario.init({
+    email: DataTypes.STRING,
+    nombre: DataTypes.STRING,
+    clave: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Usuario',
+    paranoid: true,
+  });
+  return Usuario;
+};
