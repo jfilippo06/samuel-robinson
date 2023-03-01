50f20d05c9f1178a8b9f8ad45d0a37a70bbd3b13
jfilippo06 <jfilippo04@gmail.com>
2023-03-01 01:34:14 +0100

    seeder

--- seeders/20230228190035-Noticia.js
+++ seeders/20230228190035-Noticia.js
@@ -3,15 +3,52 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
+    await queryInterface.bulkInsert(
+      "Noticia",
+      [
+        {
+          UsuarioId: 1,
+          usuario: "Admin",
+          titulo: "Very happy to find this institute!",
+          noticia: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea saepe placeat ipsam pariatur fuga nesciunt quae corporis quisquam quam voluptate explicabo at error, autem corrupti id doloremque consectetur, ipsum rem!",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          UsuarioId: 1,
+          usuario: "Admin",
+          titulo: "Very happy to find this institute!",
+          noticia: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea saepe placeat ipsam pariatur fuga nesciunt quae corporis quisquam quam voluptate explicabo at error, autem corrupti id doloremque consectetur, ipsum rem!",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          UsuarioId: 1,
+          usuario: "Admin",
+          titulo: "Very happy to find this institute!",
+          noticia: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea saepe placeat ipsam pariatur fuga nesciunt quae corporis quisquam quam voluptate explicabo at error, autem corrupti id doloremque consectetur, ipsum rem!",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          UsuarioId: 1,
+          usuario: "Admin",
+          titulo: "Very happy to find this institute!",
+          noticia: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea saepe placeat ipsam pariatur fuga nesciunt quae corporis quisquam quam voluptate explicabo at error, autem corrupti id doloremque consectetur, ipsum rem!",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          UsuarioId: 1,
+          usuario: "Admin",
+          titulo: "Very happy to find this institute!",
+          noticia: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea saepe placeat ipsam pariatur fuga nesciunt quae corporis quisquam quam voluptate explicabo at error, autem corrupti id doloremque consectetur, ipsum rem!",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   async down(queryInterface, Sequelize) {
